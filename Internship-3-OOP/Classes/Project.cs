d102522ec428354bc17d5fbeff28bdf8abed3d19
ivonaaaa <ivonaa.e@gmail.com>
2024-11-22 00:10:37 +0100

    Added classes

---
+++ Internship-3-OOP/Classes/Project.cs
@@ -0,0 +1,41 @@
+﻿using Internship_3_OOP.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Internship_3_OOP.Classes
+{
+    internal class Project
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDate { get; private set; }
+        public DateTime EndDate { get; private set; }
+        public ProjectStatus Status { get; set; }
+        public List<Task> Tasks { get; set; }
+
+        public Project(string name, string description, DateTime startDate, DateTime endDate, ProjectStatus status)
+        {
+            this.Name = name;
+            this.Description = description;
+            this.StartDate = startDate;
+            this.EndDate = endDate;
+            this.Status = status;
+            this.Tasks = new List<Task>();
+        }
+
+        public void SetStatus(ProjectStatus newStatus)
+        {
+            if (Status != ProjectStatus.Completed)
+            {
+                Status = newStatus;
+            }
+            else
+            {
+                Console.WriteLine("Nije moguće promijeniti status završenog projekta.");
+            }
+        }
+    }
+}
---
+++ Internship-3-OOP/Classes/Task.cs
@@ -0,0 +1,39 @@
+﻿using Internship_3_OOP.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Internship_3_OOP.Classes
+{
+    internal class Task
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
+        public Enum.TaskStatus Status { get; set; }
+        public int ExpectedDuration { get; set; }
+
+        public Task(string name, string description, DateTime dueDate, Enum.TaskStatus status, int expectedDuration)
+        {
+            this.Name = name;
+            this.Description = description;
+            this.DueDate = dueDate;
+            this.Status = status;
+            this.ExpectedDuration = expectedDuration;
+        }
+
+        public void SetStatus(Enum.TaskStatus newStatus)
+        {
+            if (Status != Enum.TaskStatus.Completed)
+            {
+                Status = newStatus;
+            }
+            else
+            {
+                Console.WriteLine("Nije moguće promijeniti status završenog zadatka.");
+            }
+        }
+    }
+}
